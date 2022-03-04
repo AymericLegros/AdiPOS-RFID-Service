23a3ad65c34e2450318488bfa4f8db235f91c69d
Aymeric <aymeric.legros@adixon.fr>
2022-03-04 13:30:27 +0100

    clean

--- app/src/main/java/fr/adixon/adiposrfid/MyService.kt
+++ app/src/main/java/fr/adixon/adiposrfid/MyService.kt
@@ -1,22 +1,19 @@
 package fr.adixon.adiposrfid
 
 import android.app.Service
-import android.util.Log
 import android.content.Intent
 import android.os.*
-import java.lang.Thread
-import java.lang.InterruptedException
-import android.widget.Toast
+import android.util.Log
 import java.util.*
 
 class MyService : Service() {
     var randomNumber = 0
-        private set
     private var mIsRandomGeneratorOn = false
-    private val MIN = 0
-    private val MAX = 100
 
-    private inner class RandomNumberRequestHandler : Handler() {
+    private lateinit var mMessenger: Messenger
+    private var bIntent: Intent = Intent("fr.adixon.adiposrfid.MyService.BROADCAST_ACTION");
+
+    private inner class IncomingHandler : Handler() {
         override fun handleMessage(msg: Message) {
             when (msg.what) {
                 GET_RANDOM_NUMBER_FLAG -> {
@@ -33,37 +30,53 @@ class MyService : Service() {
         }
     }
 
-    private val randomNumberMessenger = Messenger(RandomNumberRequestHandler())
-    override fun onBind(intent: Intent): IBinder? {
-        return randomNumberMessenger.binder
-    }
-
-    override fun onRebind(intent: Intent) {
-        super.onRebind(intent)
-    }
-
-    override fun onStart(intent: Intent, startId: Int) {
-        super.onStart(intent, startId)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        stopRandomNumberGenerator()
+    override fun onCreate() {
+        super.onCreate()
+        // The service is being created
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        // The service is starting, due to a call to startService()
         mIsRandomGeneratorOn = true
         Thread { startRandomNumberGenerator() }.start()
         return START_STICKY
     }
+
+    override fun onBind(intent: Intent): IBinder? {
+        // A client is binding to the service with bindService()
+        mMessenger = Messenger(IncomingHandler())
+        return mMessenger.binder
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        // All clients have unbound with unbindService()
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent) {
+        // A client is binding to the service with bindService(),
+        // after onUnbind() has already been called
+        super.onRebind(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("--------- onDestroy ---------")
+        mIsRandomGeneratorOn = false
+        // The service is no longer used and is being destroyed
+    }
+
 
     private fun startRandomNumberGenerator() {
         while (mIsRandomGeneratorOn) {
             try {
                 Thread.sleep(1000)
                 if (mIsRandomGeneratorOn) {
-                    randomNumber = Random().nextInt(MAX) + MIN
+                    val randomNumber: Int = Random().nextInt(100)
                     Log.i(TAG, "Random Number: $randomNumber")
+
+                    bIntent.putExtra("data", "Random Number: $randomNumber")
+                    sendBroadcast(bIntent);
                 }
             } catch (e: InterruptedException) {
                 Log.i(TAG, "Thread Interrupted")
@@ -71,14 +84,6 @@ class MyService : Service() {
         }
     }
 
-    private fun stopRandomNumberGenerator() {
-        mIsRandomGeneratorOn = false
-        Toast.makeText(applicationContext, "Service Stopped", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onUnbind(intent: Intent): Boolean {
-        return super.onUnbind(intent)
-    }
 
     companion object {
         private val TAG = MyService::class.java.simpleName
